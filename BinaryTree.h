f2679515286be903d7781e9adbc38483ced4da0d
Ahmed Elshafey <ahmed.elshafey47@gmail.com>
2017-11-06 20:04:14 +0200

    initial pass on BinaryTree.h

--- BinaryTree.h
+++ BinaryTree.h
@@ -15,80 +15,35 @@
 
 template <class keyType, class dataType>
 
-class binaryTree
-{   
-
-   public:
-	        
-      
-      
-      binaryTree();
-
-      
-      bool insert (const keyType &, const dataType &);
-
-      
-      bool empty() const;
-
-      
-      bool search (const keyType &) const;
-
-	  
-      bool retrieve (const keyType &, dataType &) const;
-	  
-	  
-      void traverse() const;
-
-	  
-	  void preorder () const;
-
-	  
-	  void levelorder () const;
-
-	  
-      void graph() const;
-
-	  
-      void remove (const keyType &);
-
-      
-   private:
-      
-	   class treeNode
-	   {
-		public:
-			keyType key; 		
-			dataType data;		
-			treeNode *left;		
-			treeNode *right;	
-	   }; 
-
-	   typedef treeNode * NodePointer;
-	 
-      NodePointer root;
-	  
-      
-
-      
-      bool search2 (NodePointer , const keyType &) const;
-	  
-      
-      bool retrieve2 (NodePointer , const keyType & , dataType &) const;
-	  
-	  
-      bool insert2 (NodePointer &, const keyType &, const dataType &);
-
-	  
-      void traverse2 (NodePointer ) const; 
-
-	  
-	  void graph2 (int ,NodePointer ) const;
-
-	  
-      void parentSearch ( const keyType &k, bool &found, 
-							NodePointer &locptr, NodePointer &parent) const;
-
-
+class binaryTree {   
+    public:
+        binaryTree();  
+        bool insert (const keyType &, const dataType &);
+        bool empty() const;
+        bool search (const keyType &) const;
+        bool retrieve (const keyType &, dataType &) const;
+        void traverse() const;
+        void preorder () const;
+        void levelorder () const;
+        void graph() const;
+        void remove (const keyType &);
+    private:
+        class treeNode {
+            public:
+                keyType key; 		
+                dataType data;		
+                treeNode *left;		
+                treeNode *right;	
+        }; 
+        typedef treeNode * NodePointer;
+        NodePointer root;
+	bool search2 (NodePointer , const keyType &) const;
+	bool retrieve2 (NodePointer , const keyType & , dataType &) const;
+	bool insert2 (NodePointer &, const keyType &, const dataType &);
+        void traverse2 (NodePointer ) const; 
+        void graph2 (int ,NodePointer ) const;
+        void parentSearch 
+        ( const keyType &k, bool &found, NodePointer &locptr, NodePointer &parent) const;
 }; 
 
 #endif 
